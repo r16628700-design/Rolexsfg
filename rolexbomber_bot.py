235d39941c255dc6091a2c9b20f44e1f2439e2ee
r16628700-design <r16628700@gmail.com>
2026-08-05 22:37:12 +0530

    rolexbomber_bot.py

--- rolexbomber_bot.py
+++ rolexbomber_bot.py
@@ -132,7 +132,7 @@ from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboard
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
 
 # ========== VPS ENVIRONMENT VARIABLES ==========
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8390444092:AAF11Znyxs1ayPuDG7DlCcsSQFVnmpGErus")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8495053693:AAH28HAuqT_b5jtshK3UKweTpH5dnVgCXPo")
 OWNER_ID = int(os.environ.get("OWNER_ID", 8128821116))
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 8128821116))
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "https://kommodo.ai/i/wqVc4u68cErtebyE6okA")
